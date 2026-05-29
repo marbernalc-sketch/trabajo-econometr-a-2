30baa3914227e7732ecf9599e2aa16d6ba00fac7
marbernalc-sketch <marbernalc@unal.edu.co>
2026-05-28 20:48:27 -0500

    cambios código ipc

--- econometria_ipc.py
+++ econometria_ipc.py
@@ -5,6 +5,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from pathlib import Path
 from scipy.stats import jarque_bera, probplot
 from statsmodels.tsa.arima.model import ARIMA
@@ -23,35 +24,38 @@ def cargar_datos():
     BASE_DIR = Path(__file__).resolve().parent
     DATA_DIR = BASE_DIR / "datos"
     data_path = DATA_DIR / "dato_posterIPC.csv"
-    df = pd.read_csv(data_path, header=0, names=['Fecha', 'Valor'], parse_dates=[0])
+    df = pd.read_csv(data_path, header=0, names=['Fecha', 'Valor'], parse_dates=['Fecha'],)
+    df.set_index('Fecha', inplace=True)
     print(df.head())
     return df
 # %%==============================
-# índice de precios al consumidor
-# 3. Gráfico de la serie original
+# índice de precios al consumidor- USA
 
+# 3. Gráfico de la serie original
 def graficar_serie(df_ipc):
     BASE_DIR = Path(__file__).resolve().parent
     plt.style.use('seaborn-v0_8')
     sns.set_palette("husl")
     plt.figure(figsize=(12, 6))
-    plt.plot(df_ipc.index, df_ipc['Valor'], label='Índice de Precios al Consumidor')
-    plt.xlabel('Fecha')
-    plt.ylabel('Valor')
-    plt.title('Serie Temporal del Índice de Precios al Consumidor')
-    plt.legend()
+    plt.plot(df_ipc.index, df_ipc['Valor'], label='Índice de Precios al Consumidor en EE.UU (1955-presente) ')
+    plt.xlabel('Año')
+    plt.ylabel('Variación Mensual del IPC (%)')
+    plt.title('Serie Temporal del Índice de Precios al Consumidor - EE.UU')
+    ticks = np.arange(1945, df_ipc.index.max().year + 1, 10)
+    plt.xticks(ticks=ticks, rotation=45)
+    plt.xlim(df_ipc.index.min(), df_ipc.index.max())
     plt.savefig(BASE_DIR / "serie_original.png")  
     plt.close()  
 
 
-#  Gráficos ACF y PACF 
-def graficar_acf_pacf(serie, sufijo="", archivo="acf_pacf.png"):
+#  Gráficos FAC y FACP de la serie original 
+def graficar_fac_facp(serie, sufijo="", archivo="fac_facp.png"):
     fig, axes = plt.subplots(1, 2, figsize=(15, 5))
     BASE_DIR = Path(__file__).resolve().parent
     plot_acf(serie, ax=axes[0], lags=40, title="Función de Autocorrelación (ACF)")
     plot_pacf(serie, ax=axes[1], lags=40, title="Función de Autocorrelación Parcial (PACF)")
     plt.tight_layout()
-    plt.savefig(BASE_DIR / "acf_pacf_original.png")
+    plt.savefig(BASE_DIR / archivo)
     plt.close()
     print(f"Guardada: {archivo}")
 
@@ -73,12 +77,8 @@ def logaritmo_y_graficar(df_ipc):
     plt.savefig(BASE_DIR / "serie_logaritmica.png")
     plt.close()
     
-    # FAC, FACP y ADF de la serie logarítmica
-    graficar_acf_pacf(ipc_serie_log, "Logarítmica", "acf_pacf_log.png")
-    BASE_DIR = Path(__file__).resolve().parent
-    plt.savefig(BASE_DIR / "fac_facp_log.png")
-
-    
+    # FAC, FACP de la serie logarítmica
+    graficar_fac_facp(ipc_serie_log.dropna().reset_index(drop=True), "Logarítmica", "fac_facp_log.png")
     return ipc_serie_log
 
 # 5. Prueba de estacionariedad (ADF)
@@ -103,43 +103,17 @@ def prueba_kpss(serie, nombre="Serie"):
     for nivel, valor in kpss_result[3].items():
         print(f"{nivel}: {valor}")
 
-# Diferenciando la serie para hacerla estacionaria
-def diferenciar_y_graficar(df_ipc):
-    df_ipc_serie_log_diff = df_ipc['Valor'].diff().dropna() 
-    plt.figure(figsize=(12, 6))
-    plt.plot(df_ipc_serie_log_diff.index, df_ipc_serie_log_diff, label='Serie Diferenciada')
-    plt.xlabel('Fecha')
-    plt.ylabel('Valor')
-    plt.title('Serie Temporal Diferenciada')
-    plt.legend()
-    BASE_DIR = Path(__file__).resolve().parent
-    plt.savefig(BASE_DIR / "serie_log_diferenciada.png")
-    plt.close()
-    return df_ipc_serie_log_diff 
-
-# Graficar FAC y FACP de la serie diferenciada
-def graficar_acf_pacf_diferenciada(serie, sufijo="", archivo="fac_facp_diferenciada.png"):
-    fig, axes = plt.subplots(1, 2, figsize=(15, 5))
-    BASE_DIR = Path(__file__).resolve().parent
-    plot_acf(serie, ax=axes[0], lags=40, title="Función de Autocorrelación (ACF) - Diferenciada")
-    plot_pacf(serie, ax=axes[1], lags=40, title="Función de Autocorrelación Parcial (PACF) - Diferenciada")
-    plt.tight_layout()
-    plt.savefig(BASE_DIR / "fac_facp_diferenciada.png")
-    plt.close()
-    print(f"Guardada: {archivo}")
-
 
 if __name__ == "__main__":
     df_ipc = cargar_datos()
     graficar_serie(df_ipc)
-    graficar_acf_pacf(df_ipc['Valor'], "Original", "acf_pacf_original.png")
+    graficar_fac_facp(df_ipc['Valor'], "Original", "fac_facp_original.png")
     prueba_adf(df_ipc['Valor'], "Original")
     prueba_kpss(df_ipc['Valor'], "Original")
-    df_ipc_diff = diferenciar_y_graficar(df_ipc)
-    graficar_acf_pacf_diferenciada(df_ipc_diff, "Diferenciada", "fac_facp_diferenciada.png")
-    prueba_adf(df_ipc_diff, "Diferenciada")
-    prueba_kpss(df_ipc_diff, "Diferenciada")
-    ipc_serie_log = logaritmo_y_graficar(df_ipc)          
+    ipc_serie_log = logaritmo_y_graficar(df_ipc)
+    from pathlib import Path
+    print(Path(__file__).resolve().parent)
+    
 
 
 
